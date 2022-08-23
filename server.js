c9cd463392066fd10b67ce3c04797f061e98930a
Sujung Yeo <sujungyeo@Sujungui-MacBookPro.local>
2022-08-23 15:01:39 +1200

    adding data.json and adding post and middleware

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
-const express = require("express");
-
+const express = require('express');
+const cors = require("cors");
 const app = express();
+const fs = require("fs");
+
+app.use(cors());
+app.use(express.json());
+
+
+app.post('/pizza', (req, res) => {   
+    const rawData = fs.readFileSync("./data/data.json");
+
+    const data = (JSON.parse(rawData)); // changing JSON to javascript object//decode
+
+    data.pizza.push(req.body);
+
+    const newJSON = JSON.stringify(data);// changing java object to JSON//encode
+
+    fs.writeFileSync("./data/data.json", newJSON);// saving data to JSON
+
+
+    res.send(req.body);
+});
+
+
 
 app.listen(3000);
 
-//made change by grace hahahahahaha
+
 
